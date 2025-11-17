fa4bc86ac60e053065795f2119aecb2230227f8c
jincheol5 <oscar4293@gmail.com>
2025-11-17 20:09:44 +0900

    update

--- app/app_data.py
+++ app/app_data.py
@@ -12,7 +12,7 @@ def app_data(config: dict):
         case 1:
             """
             App 1.
-            Convert and save graph_list_dict into dataset_list and store them in chunks.
+            Convert and save graph_list_dict into dataseq_list and store them in chunks.
             All type graph
             train_20
             val_20
@@ -20,32 +20,19 @@ def app_data(config: dict):
             """
             graph_list_dict=DataUtils.load_from_pickle(file_name=f"{config['mode']}_{config['num_nodes']}",path="trne",dir_type="graph")
             dataset_list_dict=GraphUtils.convert_to_dataset_list_dict(graph_list_dict=graph_list_dict)
-            all_dataset_list=[]
-            for _,dataset_list in dataset_list_dict.items():
-                all_dataset_list+=dataset_list
-            
-            match config['mode']:
-                case 'train':
-                    DataUtils.save_to_dataset_list(
-                        dataset_list=all_dataset_list,
-                        mode=config['mode'],
-                        num_nodes=config['num_nodes'],
-                        chunk_size=config['chunk_size'],
-                        dir_type=config['mode']
-                    )
-                case 'val'|'test':
-                    DataUtils.save_to_dataseq_list(
-                        dataset_list=all_dataset_list,
-                        mode=config['mode'],
-                        num_nodes=config['num_nodes'],
-                        chunk_size=config['chunk_size'],
-                        dir_type=config['mode']
-                    )
+            DataUtils.save_dataset_list_dict_to_dataseq_list(
+                dataset_list_dict=dataset_list_dict,
+                random_src=config['random_src'],
+                mode=config['mode'],
+                num_nodes=config['num_nodes'],
+                chunk_size=config['chunk_size'],
+                dir_type=config['mode']
+            )
 
         case 2:
             """
             App 2.
-            Convert and save graph_list_dict into dataset_list and store them in chunks.
+            Convert and save graph_list_dict into dataseq_list and store them in chunks.
             Only one_type graph
             test_50
             test_100
@@ -56,23 +43,14 @@ def app_data(config: dict):
             graph_list=graph_list_dict[config['graph_type']]
             dataset_list=GraphUtils.convert_to_dataset_list(graph_list=graph_list,graph_type=config['graph_type'])
 
-            match config['mode']:
-                case 'train':
-                    DataUtils.save_to_dataset_list(
-                        dataset_list=all_dataset_list,
-                        mode=config['mode'],
-                        num_nodes=config['num_nodes'],
-                        chunk_size=config['chunk_size'],
-                        dir_type=config['mode']
-                    )
-                case 'val'|'test':
-                    DataUtils.save_to_dataseq_list(
-                        dataset_list=all_dataset_list,
-                        mode=config['mode'],
-                        num_nodes=config['num_nodes'],
-                        chunk_size=config['chunk_size'],
-                        dir_type=config['mode']
-                    )
+            DataUtils.save_dataset_list_to_dataseq_list(
+                dataset_list=dataset_list,
+                mode=config['mode'],
+                num_nodes=config['num_nodes'],
+                chunk_size=config['chunk_size'],
+                dir_type=config['mode']
+            )
+
 
 if __name__=="__main__":
     """
@@ -81,6 +59,7 @@ if __name__=="__main__":
     parser=argparse.ArgumentParser()
     # app number
     parser.add_argument("--app_num",type=int,default=1)
+    parser.add_argument("--random_src",type=int,default=0)
     parser.add_argument("--mode",type=str,default='test')
     parser.add_argument("--num_nodes",type=int,default=20)
     parser.add_argument("--graph_type",type=str,default='default')
@@ -90,6 +69,7 @@ if __name__=="__main__":
     config={
         # app 관련
         'app_num':args.app_num,
+        'random_src':args.random_src,
         'mode':args.mode,
         'num_nodes':args.num_nodes,
         'graph_type':args.graph_type,
--- tgnn/data_utils.py
+++ tgnn/data_utils.py
@@ -1,4 +1,5 @@
 import os
+import random
 import pickle
 import torch
 from tqdm import tqdm
@@ -31,35 +32,50 @@ class DataUtils:
         return data
 
     @staticmethod
-    def save_to_dataset_list(dataset_list:list,mode:Literal['train'],num_nodes:int,chunk_size:int,dir_type:Literal['train','val','test']):
-        """
-        dataset 단위 리스트로 저장
-        """
-        file_path=os.path.join(DataUtils.tgnn_path,dir_type)
-        exist_chunk_files=[f for f in os.listdir(file_path) if f.startswith(f"{mode}_{num_nodes}_chunk_dataset_{chunk_size}_")]
-        idx_offset=len(exist_chunk_files)
-        chunk_list=[dataset_list[i:i+chunk_size] for i in range(0,len(dataset_list),chunk_size)]
-        for idx,chunk in tqdm(enumerate(chunk_list),total=len(chunk_list),desc=f"Saving {mode}_{num_nodes}_chunk_dataset_{chunk_size}..."):
-            DataUtils.save_to_pickle(data=chunk,file_name=f"{mode}_{num_nodes}_chunk_dataset_{chunk_size}_{idx+idx_offset}",path='tgnn',dir_type=dir_type)
-        print(f"Save {mode}_{num_nodes}_chunk_dataset_{chunk_size}!")
-
-    @staticmethod
-    def save_to_dataseq_list(dataset_list:list,mode:Literal['val','test'],num_nodes:int,chunk_size:int,dir_type:Literal['train','val','test']):
-        """
-        dataseq 단위 리스트로 저장
-        """
+    def save_dataset_list_to_dataseq_list(dataset_list:list,mode:Literal['train','val','test'],num_nodes:int,chunk_size:int,dir_type:Literal['train','val','test']):
         dataseq_list=[]
         for dataset in dataset_list:
-            dataseq_list+=[data for dataseq in dataset for data in dataseq]
+            dataseq_list+=[dataseq for dataseq in dataset]
         
         file_path=os.path.join(DataUtils.tgnn_path,dir_type)
-        exist_chunk_files=[f for f in os.listdir(file_path) if f.startswith(f"{mode}_{num_nodes}_chunk_dataseq_{chunk_size}_")]
+        exist_chunk_files=[f for f in os.listdir(file_path) if f.startswith(f"{mode}_{num_nodes}_chunk_{chunk_size}_")]
         idx_offset=len(exist_chunk_files)
 
         chunk_list=[dataseq_list[i:i+chunk_size] for i in range(0,len(dataseq_list),chunk_size)]
-        for idx,chunk in tqdm(enumerate(chunk_list),total=len(chunk_list),desc=f"Saving {mode}_{num_nodes}_chunk_dataseq_{chunk_size}..."):
-            DataUtils.save_to_pickle(data=chunk,file_name=f"{mode}_{num_nodes}_chunk_dataseq_{chunk_size}_{idx+idx_offset}",path='tgnn',dir_type=dir_type)
-        print(f"Save {mode}_{num_nodes}_chunk_dataseq_{chunk_size}!")
+        for idx,chunk in tqdm(enumerate(chunk_list),total=len(chunk_list),desc=f"Saving {mode}_{num_nodes}_chunk_{chunk_size}..."):
+            DataUtils.save_to_pickle(data=chunk,file_name=f"{mode}_{num_nodes}_chunk_{chunk_size}_{idx+idx_offset}",path='tgnn',dir_type=dir_type)
+        print(f"Save {mode}_{num_nodes}_chunk_{chunk_size}!")
+
+    @staticmethod
+    def save_dataset_list_dict_to_dataseq_list(dataset_list_dict:dict,random_src:int,mode:Literal['train','val','test'],num_nodes:int,chunk_size:int,dir_type:Literal['train','val','test']):
+        if random_src:
+            dataseq_list=[]
+            source_info_dict={}
+            for graph_type,dataset_list in dataset_list_dict.items():
+                source_id_list=[]
+                for dataset in dataset_list:
+                    random_src_id=random.randrange(num_nodes)
+                    source_id_list.append(random_src_id)
+                    dataseq_list.append(dataset[random_src_id])
+                source_info_dict[graph_type]=source_id_list
+            
+            # save source_info_dict
+            DataUtils.save_to_pickle(data=source_info_dict,file_name=f"source_info_dict",path='tgnn',dir_type=dir_type)
+
+            # save dataseq_list
+            file_path=os.path.join(DataUtils.tgnn_path,dir_type)
+            exist_chunk_files=[f for f in os.listdir(file_path) if f.startswith(f"{mode}_{num_nodes}_chunk_{chunk_size}_")]
+            idx_offset=len(exist_chunk_files)
+
+            chunk_list=[dataseq_list[i:i+chunk_size] for i in range(0,len(dataseq_list),chunk_size)]
+            for idx,chunk in tqdm(enumerate(chunk_list),total=len(chunk_list),desc=f"Saving {mode}_{num_nodes}_chunk_{chunk_size}..."):
+                DataUtils.save_to_pickle(data=chunk,file_name=f"{mode}_{num_nodes}_chunk_{chunk_size}_{idx+idx_offset}",path='tgnn',dir_type=dir_type)
+            print(f"Save {mode}_{num_nodes}_chunk_{chunk_size}!")
+        else:
+            all_dataset_list=[]
+            for _,dataset_list in dataset_list_dict.items():
+                all_dataset_list+=dataset_list
+            DataUtils.save_dataset_list_to_dataseq_list(dataset_list=all_dataset_list,mode=mode,num_nodes=num_nodes,chunk_size=chunk_size,dir_type=dir_type)
 
     @staticmethod
     def save_model_parameter(model,model_name:str):
